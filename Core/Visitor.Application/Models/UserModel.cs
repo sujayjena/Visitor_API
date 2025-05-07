5c7dac412f3f8e12f5d38f6ecaaf348a71abc89c
nazbullah-netswift <nazbullah.netswift2021@gmail.com>
2025-05-07 15:29:54 +0530

    branchname issue fixed

--- Core/Visitor.Application/Models/UserModel.cs
+++ Core/Visitor.Application/Models/UserModel.cs
@@ -284,6 +284,7 @@ namespace Visitor.Application.Models
         public string OtherProofImageURL { get; set; }
 
         public int? BranchId { get; set; }
+        public string? BranchName { get; set; }
 
         public bool? IsHOD { get; set; }
 
