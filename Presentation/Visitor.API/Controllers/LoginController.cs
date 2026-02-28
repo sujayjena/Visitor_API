a5c297f2dfd623c7dee1ce6772cd30a0b1142cf6
nazbullah-netswift <nazbullah.netswift2021@gmail.com>
2026-02-28 23:01:01 +0530

    login condition

--- Presentation/Visitor.API/Controllers/LoginController.cs
+++ Presentation/Visitor.API/Controllers/LoginController.cs
@@ -150,7 +150,7 @@ namespace Visitor.API.Controllers
                     return _response;
                 }
 
-                if (Convert.ToDateTime(DateTime.Now) < Convert.ToDateTime("2026-03-05"))
+                if (Convert.ToDateTime(DateTime.Now) < Convert.ToDateTime("2026-04-07"))
                 {
                     if (loginResponse.IsActive == true && (loginResponse.IsWebUser == true && parameters.IsWebOrMobileUser == "W" || loginResponse.IsMobileUser == true && parameters.IsWebOrMobileUser == "M"))
                     {
